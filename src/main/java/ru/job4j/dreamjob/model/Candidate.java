7f04f48b55f884499faffff16bcb728cf5c8245a
Alexander Slepchenko <asander@list.ru>
2023-05-12 23:17:46 +0300

    6. Создание, просмотр, редактирование и удаление кандидатов [#504858]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -10,12 +10,16 @@ public class Candidate {
 
     private String description;
 
-    private LocalDateTime creationDate;
+    private LocalDateTime creationDate = LocalDateTime.now();
 
-    public Candidate(int id, String name, String description) {
+    public Candidate() {
+    }
+
+    public Candidate(int id, String name, String description, LocalDateTime creationDate) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.creationDate = creationDate;
     }
 
     public int getId() {
--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -3,6 +3,7 @@ package ru.job4j.dreamjob.repository;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Candidate;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,11 +18,11 @@ public class MemoryCandidateRepository implements CandidateRepository {
     private final Map<Integer, Candidate> candidates = new HashMap<>();
 
     private MemoryCandidateRepository() {
-        save(new Candidate(0, "Ivanov", "description1"));
-        save(new Candidate(0, "Petrov", "description2"));
-        save(new Candidate(0, "Sidorov", "description3"));
-        save(new Candidate(0, "Alexandrov", "description4"));
-        save(new Candidate(0, "Sergeev", "description5"));
+        save(new Candidate(0, "Ivanov", "description1", LocalDateTime.now()));
+        save(new Candidate(0, "Petrov", "description2", LocalDateTime.now()));
+        save(new Candidate(0, "Sidorov", "description3", LocalDateTime.now()));
+        save(new Candidate(0, "Alexandrov", "description4", LocalDateTime.now()));
+        save(new Candidate(0, "Sergeev", "description5", LocalDateTime.now()));
     }
 
     public static MemoryCandidateRepository getInstance() {
@@ -43,7 +44,8 @@ public class MemoryCandidateRepository implements CandidateRepository {
     @Override
     public boolean update(Candidate candidate) {
         return candidates.computeIfPresent(candidate.getId(), (id, oldVacancy) ->
-                new Candidate(oldVacancy.getId(), candidate.getName(), candidate.getDescription())) != null;
+                new Candidate(oldVacancy.getId(), candidate.getName(), candidate.getDescription(),
+                        candidate.getCreationDate())) != null;
     }
 
     @Override
