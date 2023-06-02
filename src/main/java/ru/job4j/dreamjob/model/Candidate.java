db65e79fe08b4b60e8df32385a66b8f4c0c45258
Alexander Slepchenko <asander@list.ru>
2023-06-02 22:05:47 +0300

    2. Формы. Списки. [#504854  [#504854]]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -14,15 +14,18 @@ public class Candidate {
 
     private boolean visible;
 
+    private int cityId;
+
     public Candidate() {
     }
 
-    public Candidate(int id, String name, String description, LocalDateTime creationDate, boolean visible) {
+    public Candidate(int id, String name, String description, LocalDateTime creationDate, boolean visible, int cityId) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.creationDate = creationDate;
         this.visible = visible;
+        this.cityId = cityId;
     }
 
     public int getId() {
@@ -63,6 +66,14 @@ public class Candidate {
 
     public void setVisible(boolean visible) {
         this.visible = visible;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
     }
 
     @Override
