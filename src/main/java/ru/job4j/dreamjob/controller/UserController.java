0b68e44d6ff7bb74862d8afb064e175a6f5ba5c7
Alexander Slepchenko <asander@list.ru>
2023-06-21 14:11:14 +0300

    4. Многопоточность в базе данных [#504860  [#504860]]

---
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -0,0 +1,29 @@
+package ru.job4j.dreamjob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.dreamjob.service.UserService;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String getRegistrationPage(Model model) {
+        model.addAttribute("users");
+        return "users/register";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "users/list";
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/service/SimpleUserService.java
@@ -0,0 +1,27 @@
+package ru.job4j.dreamjob.service;
+
+import ru.job4j.dreamjob.model.User;
+import ru.job4j.dreamjob.repository.UserRepository;
+
+import java.util.Optional;
+
+public class SimpleUserService implements UserService {
+
+    private final UserRepository userRepository;
+
+    public SimpleUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Optional<User> save(User user) {
+        Optional<User> optionalUser = Optional.ofNullable(user);
+        userRepository.save(optionalUser.get());
+        return optionalUser;
+    }
+
+    @Override
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        return userRepository.findByEmailAndPassword(email, password);
+    }
+}
